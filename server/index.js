800ce557467464d03bb1859b51148bb6bc5d5a8b
Timothy Sharkey <Techne3@gmail.com>
2020-02-22 15:37:30 -0700

    set up mongoose

--- server/index.js
+++ server/index.js
@@ -1,33 +1,46 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
+
+const Post = require("./models/Post");
 const { MONGODB } = require("./config");
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello World!"
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 };
 
 const server = new ApolloServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers
+  typeDefs,
+  resolvers
 });
 
-// in mongoDB site click on connectApp in your cluster to get this connect stings
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log("Mongodb connected");
+    console.log("MongoDB Connected");
     return server.listen({ port: 5000 });
   })
   .then(res => {
-    console.log(`server is running on ${res.url}`);
+    console.log(`Server running at ${res.url}`);
   });
-
-// node index to run server
